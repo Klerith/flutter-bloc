7d3a2e1cf6fe8535588e15a21d700ccc230d22ea
Fernando Herrera <fernando.herrera85@gmail.com>
2023-08-23 16:52:26 -0400

    Screen renombrada

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,6 +1,6 @@
 export '01_simple_cubit/cubit_screen.dart';
 export '02_cubit_router/router_screen.dart';
-export '03_multiple_cubits/state_provider_screen.dart';
+export '03_multiple_cubits/multiple_cubits_screen.dart';
 export '04_guest/guests_screen.dart';
 export '05_pokemon/pokemon_screen.dart';
 export '06_private/private_screen.dart';
