378abde1e126c22e636461ffe851c671ec879243
Fernando Herrera <fernando.herrera85@gmail.com>
2023-08-24 13:53:22 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp.router(
-      title: 'Riverpod Providers',
+      title: 'Flutter BLoC',
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
       theme: AppTheme( isDarkmode: false ).getTheme(),
