1372127f8b05c446c2288bf44c1b445af23ca203
Fernando Herrera <fernando.herrera85@gmail.com>
2023-08-24 09:31:05 -0400

    +1 y -1 botones agregados

--- lib/presentation/screens/05_pokemon/pokemon_screen.dart
+++ lib/presentation/screens/05_pokemon/pokemon_screen.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
-
 class PokemonScreen extends StatelessWidget {
   const PokemonScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bloc con Futures'),
-      ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Bloc con Futures'),
+        ),
+        body: const Center(
+          child: Text('Fernando Herrera'),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              heroTag: 'btn-add',
+              child: const Icon(Icons.plus_one),
+              onPressed: () {},
+            ),
+
+            const SizedBox(height: 15),
+
+            FloatingActionButton(
+              heroTag: 'btn-minus',
+              child: const Icon(Icons.exposure_minus_1),
+              onPressed: () {},
+            ),
+          ],
+        ));
   }
 }
